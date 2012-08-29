178301f63b93e31bec77e905347033763bc8d3d5
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2012-08-29 18:38:29 +0100

    changed approach to base64 decoding and other fixes

--- html/base64.js
+++ html/base64.js
@@ -23,24 +23,59 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 */
 function StringBuffer()
-{
-    this.buffer = [];
-}
+{ 
+    this.buffer = []; 
+} 
 
 StringBuffer.prototype.append = function append(string)
-{
-    this.buffer.push(string);
-    return this;
-};
+{ 
+    this.buffer.push(string); 
+    return this; 
+}; 
 
 StringBuffer.prototype.toString = function toString()
-{
-    return this.buffer.join("");
-};
+{ 
+    return this.buffer.join(""); 
+}; 
 
 var Base64 =
 {
-    codex : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_",
+    codex : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    encode : function (input)
+    {
+        var output = new StringBuffer();
+
+        var enumerator = new Utf8EncodeEnumerator(input);
+        while (enumerator.moveNext())
+        {
+            var chr1 = enumerator.current;
+
+            enumerator.moveNext();
+            var chr2 = enumerator.current;
+
+            enumerator.moveNext();
+            var chr3 = enumerator.current;
+
+            var enc1 = chr1 >> 2;
+            var enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            var enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            var enc4 = chr3 & 63;
+
+            if (isNaN(chr2))
+            {
+                enc3 = enc4 = 64;
+            }
+            else if (isNaN(chr3))
+            {
+                enc4 = 64;
+            }
+
+            output.append(this.codex.charAt(enc1) + this.codex.charAt(enc2) + this.codex.charAt(enc3) + this.codex.charAt(enc4));
+        }
+
+        return output.toString();
+    },
 
     decode : function (input)
     {
@@ -76,6 +111,63 @@ var Base64 =
     }
 }
 
+
+function Utf8EncodeEnumerator(input)
+{
+    this._input = input;
+    this._index = -1;
+    this._buffer = [];
+}
+
+Utf8EncodeEnumerator.prototype =
+{
+    current: Number.NaN,
+
+    moveNext: function()
+    {
+        if (this._buffer.length > 0)
+        {
+            this.current = this._buffer.shift();
+            return true;
+        }
+        else if (this._index >= (this._input.length - 1))
+        {
+            this.current = Number.NaN;
+            return false;
+        }
+        else
+        {
+            var charCode = this._input.charCodeAt(++this._index);
+
+            // "\r\n" -> "\n"
+            //
+            if ((charCode == 13) && (this._input.charCodeAt(this._index + 1) == 10))
+            {
+                charCode = 10;
+                this._index += 2;
+            }
+
+            if (charCode < 128)
+            {
+                this.current = charCode;
+            }
+            else if ((charCode > 127) && (charCode < 2048))
+            {
+                this.current = (charCode >> 6) | 192;
+                this._buffer.push((charCode & 63) | 128);
+            }
+            else
+            {
+                this.current = (charCode >> 12) | 224;
+                this._buffer.push(((charCode >> 6) & 63) | 128);
+                this._buffer.push((charCode & 63) | 128);
+            }
+
+            return true;
+        }
+    }
+}
+
 function Base64DecodeEnumerator(input)
 {
     this._input = input;
@@ -103,14 +195,12 @@ Base64DecodeEnumerator.prototype =
         {
             var enc1 = Base64.codex.indexOf(this._input.charAt(++this._index));
             var enc2 = Base64.codex.indexOf(this._input.charAt(++this._index));
-            var enc3 = ++this._index >= this._input.length ? 64 : Base64.codex.indexOf(this._input.charAt(this._index));
-            var enc4 = ++this._index >= this._input.length ? 64 : Base64.codex.indexOf(this._input.charAt(this._index));
+            var enc3 = Base64.codex.indexOf(this._input.charAt(++this._index));
+            var enc4 = Base64.codex.indexOf(this._input.charAt(++this._index));
 
             var chr1 = (enc1 << 2) | (enc2 >> 4);
             var chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
             var chr3 = ((enc3 & 3) << 6) | enc4;
-
-            console.log(chr1 + "," + chr2 + "," + chr3 )
 
             this.current = chr1;
 
